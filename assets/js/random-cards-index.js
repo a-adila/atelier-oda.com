e3661a08cd966f21773231c5c3aaca6e7820aaa8
Adila <219802882+a-adila@users.noreply.github.com>
2026-06-09 11:05:23 +0200

    feat: create js function to get indexes from projects

--- assets/js/random-cards-index.js
+++ assets/js/random-cards-index.js
@@ -1,5 +1,17 @@
 fetch("../fiche-projets.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    // Create a table to store random indexes
+    const randomIndexes = [];
+
+    // Get 3 random indexes
+    while (randomIndexes.length < 3) {
+      const index = Math.floor(Math.random() * data.length);
+      if (!randomIndexes.includes(index)) {
+        randomIndexes.push(index);
+      }
+    }
+    // Create an array to get indexes of projects
+    const randomProjects = randomIndexes.map((index) => data[index]);
+    console.log(randomIndexes);
   });
