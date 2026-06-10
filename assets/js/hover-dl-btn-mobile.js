cd9eace92ef64f120af6c134fcc62745aaff4415
Adila <219802882+a-adila@users.noreply.github.com>
2026-06-10 17:07:38 +0200

    chore: remove console.logs

--- assets/js/hover-dl-btn-mobile.js
+++ assets/js/hover-dl-btn-mobile.js
@@ -2,16 +2,12 @@
 // DOWNLOAD BUTTON HOVER ON MOBILE //
 /////////////////////////////////////
 const downloadButton = document.querySelector(".btn-portfolio");
-console.log("downloadButton :", downloadButton);
 if (downloadButton) {
-  console.log("window.innerWidth :", window.innerWidth);
   if (window.innerWidth < 768) {
-    console.log("On entre dans le if 768");
     let isVisible = false;
 
     const observer = new IntersectionObserver(
       (entries) => {
-        console.log("Observer déclenché", entries[0].isIntersecting);
         if (entries[0].isIntersecting) {
           isVisible = true;
         } else {
@@ -22,7 +18,6 @@ if (downloadButton) {
     );
 
     observer.observe(downloadButton);
-    console.log("Observer actif sur :", downloadButton);
 
     window.addEventListener("scroll", () => {
       if (isVisible) {
