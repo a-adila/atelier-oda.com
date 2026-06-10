9ef2b599046d382e46f18391c6485793f34e0dc6
Adila <219802882+a-adila@users.noreply.github.com>
2026-06-10 14:15:57 +0200

    feat(js, css): delete hover-mobile.js, put mobile hover script in projets.js

--- assets/js/projets.js
+++ assets/js/projets.js
@@ -1,3 +1,5 @@
+const windowWidth = window.innerWidth;
+
 fetch("../fiche-projets.json")
   .then((response) => response.json())
   .then((projets) => {
@@ -57,4 +59,43 @@ fetch("../fiche-projets.json")
         index++;
       }
     });
+
+    /////////////////////
+    // HOVER ON MOBILE //
+    /////////////////////
+
+    if (windowWidth < 768) {
+      const cards = document.querySelectorAll(".project__card");
+      let visibleCards = [];
+
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              visibleCards.push(entry.target);
+            } else {
+              visibleCards = visibleCards.filter((c) => c !== entry.target);
+            }
+          });
+        },
+        { threshold: 0 },
+      );
+
+      cards.forEach((card) => observer.observe(card));
+
+      window.addEventListener("scroll", () => {
+        visibleCards.forEach((card) => {
+          const rect = card.getBoundingClientRect();
+          const cardCenter = (rect.top + rect.bottom) / 2;
+          const screenCenter = window.innerHeight / 2;
+          const isNearCenter = Math.abs(cardCenter - screenCenter) < 150;
+
+          if (isNearCenter) {
+            card.querySelector(".picture-hover").classList.add("active");
+          } else {
+            card.querySelector(".picture-hover").classList.remove("active");
+          }
+        });
+      });
+    }
   });
